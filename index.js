da373587b72b3be0814f2754dbfb95beabd7b615
Shubz100 <115923262+Shubz100@users.noreply.github.com>
2024-10-04 16:29:16 +0530

    index.js

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-const express = require('express');
+import express from 'express';
+import api from './api';
+
 const app = express();
 
-app.use(express.json());
+app.use('/api', api);
 
-app.post('/telegram-webhook', (req, res) => {
-  console.log('Received webhook request');
-  try {
-    const update = req.body;
-    const chatId = update.message.chat.id;
-    // Process the update and send a response back to Telegram
-    res.status(200).send('OK');
-  } catch (error) {
-    console.error('Error processing webhook request:', error);
-    res.status(500).send('Error processing request');
-  }
-});
-
-app.use((req, res) => {
-  console.log('404 error: Not Found');
-  res.status(404).send('Not Found');
-});
-
-module.exports = app;
+export default app;
