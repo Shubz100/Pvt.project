59d2cc7453eafaff2f53a175a646de2a2f540e14
Shubz100 <115923262+Shubz100@users.noreply.github.com>
2024-10-04 16:15:43 +0530

    index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.post('/telegram-webhook', (req, res) => {
   console.log(req.headers);
   console.log(req.body);
 
+  const repository = req.body.repository;
+  const eventName = req.body.event_name;
+
   // Process the payload and send a response back to GitHub
   res.status(200).send('OK');
 });
